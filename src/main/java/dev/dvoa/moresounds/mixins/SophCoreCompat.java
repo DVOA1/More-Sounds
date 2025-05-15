6c065ac03a3b05bb05fd139c7a999536b68e921e
DVOA1 <88774071+DVOA1@users.noreply.github.com>
2025-05-15 16:29:44 +0200

    fix: no sound playing when quick moving and item

--- src/main/java/dev/dvoa/moresounds/mixins/SophCoreCompat.java
+++ src/main/java/dev/dvoa/moresounds/mixins/SophCoreCompat.java
@@ -14,22 +14,39 @@ import net.p3pp3rf1y.sophisticatedcore.client.gui.StorageScreenBase;
 import net.p3pp3rf1y.sophisticatedcore.common.gui.StorageContainerMenuBase;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Pseudo;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Pseudo
 @Mixin(StorageScreenBase.class)
 public abstract class SophCoreCompat<S extends StorageContainerMenuBase<?>> extends AbstractContainerScreen<S> {
+    @Unique
+    private Slot more_Sounds$lastHoveredSlot;
+
     public SophCoreCompat(S menu, Inventory playerInventory, Component title) {
         super(menu, playerInventory, title);
+    }
+
+    @Inject(method = "isHovering", at = {@At(value = "RETURN")})
+    public void $is_mouse_ov_slo(Slot slot, double mouseX, double mouseY, CallbackInfoReturnable<Boolean> cir) {
+        if (cir.getReturnValue() == true && slot != null) {
+            more_Sounds$lastHoveredSlot = slot;
+        }
     }
 
     @Inject(method = "slotClicked", at = {@At(value = "RETURN")})
     public void $on_slot_clicked(Slot slot, int slotNumber, int mouseButton, ClickType type, CallbackInfo ci) {
         if (slot != null) {
-            ItemStack stack = getMenu().getCarried();
-            if (stack.isEmpty()) stack = slot.getItem();
+            ItemStack stack;
+            if (type != ClickType.QUICK_MOVE) {
+                stack = getMenu().getCarried();
+                if (stack.isEmpty()) stack = slot.getItem();
+            } else {
+                stack = more_Sounds$lastHoveredSlot.getItem();
+            }
             SoundsConfig.get(UISoundsConfig.class).itemClickSoundEffect.playDynamicSound(stack, ItemStackSoundContext.of(DynamicSoundHelper.BlockSoundType.PLACE));
         }
     }
